e38d54f57572c532fa0d195a610200cf78585c14
Jose Tongson <kanakanadelta@gmail.com>
2018-09-19 20:04:15 -0700

    fix classnames on svgs

--- client/assets/albumFrame.jsx
+++ client/assets/albumFrame.jsx
@@ -25,13 +25,13 @@ const styles = {
 export default () => 
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 115.845 115.846">
   <g id="Symbol_3_1" data-name="Symbol 3 – 1" transform="translate(-19.314 -3.314)">
-    <g id="Rectangle_192" data-name="Rectangle 192" className="cls-1" style={styles.cls1} transform="matrix(0.174, 0.985, -0.985, 0.174, 117.795, 3.315)">
-      <rect className="cls-3" width="100" style={styles.cls3} height="100"/>
-      <rect className="cls-4" style={styles.cls4} x="0.625" y="0.625" width="98.75" height="98.75"/>
+    <g id="Rectangle_192" data-name="Rectangle 192" style={styles.cls1} transform="matrix(0.174, 0.985, -0.985, 0.174, 117.795, 3.315)">
+      <rect  width="100" style={styles.cls3} height="100"/>
+      <rect  style={styles.cls4} x="0.625" y="0.625" width="98.75" height="98.75"/>
     </g>
-    <g id="Rectangle_193" data-name="Rectangle 193" className="cls-2" style={styles.cls2} transform="matrix(0.985, 0.174, -0.174, 0.985, 36.679, 3.314)">
-      <rect className="cls-3" style={styles.cls3} width="100" height="100"/>
-      <rect className="cls-4" style={styles.cls4} x="0.625" y="0.625" width="98.75" height="98.75"/>
+    <g id="Rectangle_193" data-name="Rectangle 193"  style={styles.cls2} transform="matrix(0.985, 0.174, -0.174, 0.985, 36.679, 3.314)">
+      <rect  style={styles.cls3} width="100" height="100"/>
+      <rect  style={styles.cls4} x="0.625" y="0.625" width="98.75" height="98.75"/>
     </g>
   </g>
 </svg>
