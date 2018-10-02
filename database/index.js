fcc7c5d51da52186e516924e0f721d413b5cc08c
Katie McCulloch <katiee_mcculloch@yahoo.com>
2018-10-02 10:02:41 -0700

    updates postgres db for my user

--- database/index.js
+++ database/index.js
@@ -1,13 +1,17 @@
-const pg = require('pg');
-const Sequelize = require('sequelize');
+const pg = require("pg");
+const Sequelize = require("sequelize");
 
-const connection = new Sequelize('starkloud', 'postgres', 'Megumi91', {
-  dialect: 'postgres'
+const connection = new Sequelize("starkloud", "katiemcculloch", "", {
+  dialect: "postgres",
+  host: "localhost",
+  port: 5432
 });
 
 connection
   .authenticate()
-  .then(() => console.log('!!!!!Successfully connected to the postgres database!!!!!'))
-  .catch(err => console.log('Error connecting to the database...', err));
+  .then(() =>
+    console.log("!!!!!Successfully connected to the postgres database!!!!!")
+  )
+  .catch(err => console.log("Error connecting to the database...", err));
 
 module.exports = connection;
