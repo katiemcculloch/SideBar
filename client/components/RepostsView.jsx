61366d60ea47cb553b6a3d9d4b4576397e276c9c
Katie McCulloch <katiee_mcculloch@yahoo.com>
2018-10-08 15:00:58 -0700

    update api call to new users endpoint

--- client/components/RepostsView.jsx
+++ client/components/RepostsView.jsx
@@ -1,26 +1,25 @@
-import React, {Component} from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
-import UserEntry from './UserEntry.jsx';
-import PlaceHolderUser from './miniComponents/PlaceholderUser.jsx';
-
+import UserEntry from "./UserEntry.jsx";
+import PlaceHolderUser from "./miniComponents/PlaceholderUser.jsx";
 
 //styled-components//
-import HeaderText from '../styles/HeaderText';
-import HeaderIcon from '../styles/HeaderIcon';
-import ViewAllText from '../styles/ViewAllText';
-import UserList from '../styles/UserList';
+import HeaderText from "../styles/HeaderText";
+import HeaderIcon from "../styles/HeaderIcon";
+import ViewAllText from "../styles/ViewAllText";
+import UserList from "../styles/UserList";
 
 //SVG-->JSX
-import RepostsIcon from '../assets/repostsHeaderIcon.jsx'
+import RepostsIcon from "../assets/repostsHeaderIcon.jsx";
 
 class RepostsView extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       loading: true,
-      users: [0,1,2,3,4,5,6,7,8]
-    }
+      users: [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    };
     //function bindings
     this.handleOnLoad = this.handleOnLoad.bind(this);
     this.renderUsers = this.renderUsers.bind(this);
@@ -36,46 +35,44 @@ class RepostsView extends Component {
 
   getUsers() {
     axios
-      .get('/api/usersLiked', {
+      .get("/api/users", {
         // params: {}
       })
-      .then( ({data}) => {
-        console.log(data)
+      .then(({ data }) => {
+        console.log(data);
         this.setState({
           users: data,
           loading: false
-        }), ()=> console.log('users: ', this.state.users)
+        });
       })
-      .catch( err => console.log('error getting users...', err))
+      .catch(err => console.log("there was an error getting users...", err));
   }
 
   renderUsers() {
     if (!!this.state.loading) {
       return (
-          <UserList>        
-            <PlaceHolderUser />
-            <PlaceHolderUser />
-            <PlaceHolderUser />
-            <PlaceHolderUser />
-            <PlaceHolderUser />
-            <PlaceHolderUser />
-            <PlaceHolderUser />
-            <PlaceHolderUser />
-            <PlaceHolderUser />
-          </UserList>
-        )
+        <UserList>
+          <PlaceHolderUser />
+          <PlaceHolderUser />
+          <PlaceHolderUser />
+          <PlaceHolderUser />
+          <PlaceHolderUser />
+          <PlaceHolderUser />
+          <PlaceHolderUser />
+          <PlaceHolderUser />
+          <PlaceHolderUser />
+        </UserList>
+      );
     } else {
       return (
         <UserList>
-          {this.state.users.map(user=> {
-          return <UserEntry 
-              user={user}
-              key={user.id}
-              load={this.handleOnLoad}
-            />
-        })}
+          {this.state.users.map(user => {
+            return (
+              <UserEntry user={user} key={user.id} load={this.handleOnLoad} />
+            );
+          })}
         </UserList>
-      )
+      );
     }
   }
 
@@ -83,26 +80,22 @@ class RepostsView extends Component {
     return (
       <div>
         <h3>
-        <HeaderText>
-          <HeaderIcon>
-            <RepostsIcon />
-          </HeaderIcon>
-          <div className="headerText" style={{paddingTop:'3px'}}>
-            <span>
-            {(Math.floor(Math.random() * (10 + 999)) - 10) + ' Reposts' }
-            </span>
-            <ViewAllText>View all</ViewAllText>
-          </div>
-        </HeaderText>
-      </h3>
+          <HeaderText>
+            <HeaderIcon>
+              <RepostsIcon />
+            </HeaderIcon>
+            <div className="headerText" style={{ paddingTop: "3px" }}>
+              <span>
+                {Math.floor(Math.random() * (10 + 999)) - 10 + " Reposts"}
+              </span>
+              <ViewAllText>View all</ViewAllText>
+            </div>
+          </HeaderText>
+        </h3>
 
-      <div className="userList_div">
-        {this.renderUsers()}
+        <div className="userList_div">{this.renderUsers()}</div>
       </div>
-        
-    </div>
-      
-    )
+    );
   }
 }
 
