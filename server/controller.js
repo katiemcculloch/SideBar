80e3617cd83fee7db6369a2cc9dbd2fe9fe6ba2f
Katie McCulloch <katiee_mcculloch@yahoo.com>
2018-10-08 18:54:24 -0700

    adds users and usersPlaylists database queries

--- server/controller.js
+++ server/controller.js
@@ -1,14 +1,22 @@
 const Sequelize = require("sequelize");
+var sequelize = new Sequelize("starkloud", "", "", { dialect: "postgres" });
 
-const { Comments, Users, Songs, Playlist } = require("../database/models.js");
+const {
+  SongsPlaylists,
+  Users,
+  Songs,
+  Playlists
+} = require("../database/models.js");
 
 module.exports = {
   users: {
     get: (req, res) => {
       Users.findAll({
+        order: [[Sequelize.fn("RANDOM")]],
         limit: 9
       })
         .then(data => {
+          // console.log(data);
           res.status(200).send(data);
         })
         .catch(err => {
@@ -18,111 +26,18 @@ module.exports = {
   },
   usersPlaylists: {
     get: (req, res) => {
-      console.log(req.params);
-      Playlists.findOne({});
+      var userId = req.params.id;
+      sequelize
+        .query(
+          `SELECT * FROM Playlists a JOIN users b ON (a.userId = b.id) WHERE b.id = ${userId} LIMIT(3) `,
+          { type: Sequelize.QueryTypes.SELECT }
+        )
+        .then(data => {
+          res.send(data);
+        });
     }
   },
   userPlaylistSongs: {
     get: (req, res) => {}
   }
 };
-
-// module.exports = {
-//   relatedTracks: {
-//     get: (req, res) => {
-//       console.log("songs get");
-//       // const { tags } = req.query;
-//       Songs.findById(9000000)
-//         // order: [[Sequelize.fn("RANDOM")]],
-//         // limit: 3
-//         // })
-//         .then(tracks => {
-//           if (tracks) {
-//             res.status(200).send(tracks);
-//           } else {
-//             res.status(404).send("tracks get error");
-//           }
-//         })
-//         .catch(err => res.status(404).send(err));
-//     }
-//   },
-//   playlists: {
-//     get: (req, res) => {
-//       console.log("playlists get");
-//       // implement checking which playlists they're in
-//       Playlist.findAll({
-//         order: [[Sequelize.fn("RANDOM")]],
-//         limit: 3
-//       })
-//         .then(lists => {
-//           if (lists) {
-//             res.status(200).send(lists);
-//           } else {
-//             res.status(404).send("lists get error");
-//           }
-//         })
-//         .catch(err => res.status(404).send(err));
-//     }
-//   },
-//   usersLiked: {
-//     get: (req, res) => {
-//       console.log("users who liked get");
-//       Users.findAll({
-//         order: [[Sequelize.fn("RANDOM")]],
-//         limit: 9
-//       })
-//         .then(users => {
-//           if (users) {
-//             res.status(200).send(users);
-//           } else {
-//             res.status(404).send("users get error");
-//           }
-//         })
-//         .catch(err => res.status(404).send(err));
-//     }
-//   },
-//   usersReposts: {
-//     get: (req, res) => {
-//       console.log("users who reposted get");
-//       Users.findAll({
-//         order: [[Sequelize.fn("RANDOM")]],
-//         limit: 9
-//       })
-//         .then(users => {
-//           if (users) {
-//             res.status(200).send(users);
-//           } else {
-//             res.status(404).send("users get error");
-//           }
-//         })
-//         .catch(err => res.status(404).send(err));
-//     }
-//   }
-// };
-
-
-// WITH RECURSIVE search_graph(id, link, data, depth) AS (
-//   SELECT g.id, g.link, g.data, 1
-//   FROM graph g
-// UNION ALL
-//   SELECT g.id, g.link, g.data, sg.depth + 1
-//   FROM graph g, search_graph sg
-//   WHERE g.id = sg.link
-// )
-// SELECT * FROM search_graph;
-
-WITH RECURSIVE songs(id, title, artist, media, album_art, tags) AS (
-  SELECT u.id, u.username, u.pic 
-  FROM users u
-  UNION ALL 
-    SELECT p.title, p.cover_art, p.likes, p.reposts, p.userid
-    FROM playlists p 
-    JOIN users u ON (p.userid = u.id)
-    WHERE u.id = p.userid
-  UNION ALL
-    SELECT s.id, s.title, s.artist, s.media, s.album_art, s.tags
-    FROM songs s
-    JOIN songs_playlists sp ON (s.id = sp.songId)
-    WHERE sp.playlistId = p.id
-)
-SELECT * from songs limit(1);
